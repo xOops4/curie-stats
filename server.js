875ba7462119e944ed8e82d68da281bc65862048
Julien Burey <julien.burey@gmail.com>
2021-09-11 10:54:40 +0200

    install node

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+const PORT = process.env.PORT || 3000;
 app.set('view engine' , 'ejs')
 
 app.get('/' , (req, res) => {
@@ -13,4 +13,5 @@ app.get('/' , (req, res) => {
         })
 })
 
-app.listen(3000)
+app.listen(PORT)
+
